86e2076acd7c98d596a40d4aa68b3d89eb92a35e
Gabbe2003 <gabriell.touma@gmail.com>
2023-12-04 00:26:09 +0100

    Done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,16 +11,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-/*-----------------------------------------------------------------------------------------*/
-/*---------------------------------------MY CODE-------------------------------------------*/
-/*-----------------------------------------------------------------------------------------*/
 
 app.post('/api/fileanalyse',(req,res) => {
   
 });
 
 
-/*=========================================================================================*/
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
